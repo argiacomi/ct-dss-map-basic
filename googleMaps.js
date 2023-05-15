a4c041322715dd9e2fe91048e65a57ee56b91ad0
Drew Giacomi <andrew.giacomi@gmail.com>
2023-05-15 12:02:00 -0400

    Trying to figure out env variables

--- googleMaps.js
+++ googleMaps.js
@@ -1,4 +1,4 @@
-const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const apiKey = process.env.VITE_GOOGLE_MAPS_API_KEY;
 const center = { lat: 41.5, lng: -72.7575 };
 const zoom = 10;
 const locations = [
